30c70daea94601443f42887fe3cc18efa6d047cd
Elaine Deng <qd9069@gmail.com>
2022-04-23 19:51:24 -0700

    Added code in Engineer.js for unit test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require("../lib/Engineer");
 
-test.only("Can set GitHUb account via constructor", () => {
+test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
-test('getRole() should return "Engineer"', () => {
+test.skip('getRole() should return "Engineer"', () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
